7b22a2e6dc02916b719f9b5ce1d9cfaf645311bf
Shrey Kavi <skavi@clearpathrobotics.com>
2018-12-11 12:32:24 -0500

    Removed unused variables in RX and TX; split up TX and RX messages

--- scripts/wibotic_msg_funcs.py
+++ scripts/wibotic_msg_funcs.py
@@ -26,10 +26,6 @@ def tx_Flags(msg, data):
     msg.TX.Flags = turn_to_uint(data, 16) #turns to uint16
     return msg
 
-def tx_PowerLevel(msg, data):
-    msg.TX.PowerLevel = turn_to_uint(data, 16) #turns to uint16
-    return msg
-
 def tx_VMon3v3(msg, data):
     msg.TX.VMon3v3 = float(data)
     return msg
@@ -74,42 +70,6 @@ def tx_VMonBatt(msg, data):
     msg.TX.VMonBatt = float(data)
     return msg
 
-def tx_VMonBattProg(msg, data):
-    msg.TX.VMonBattProg = float(data)
-    return msg
-
-def tx_VRect(msg, data):
-    msg.TX.VRect = float(data)
-    return msg
-
-def tx_TBoard(msg, data):
-    msg.TX.TBoard = float(data)
-    return msg
-
-def tx_ICharger(msg, data):
-    msg.TX.ICharger = float(data)
-    return msg
-
-def tx_IBattery(msg, data):
-    msg.TX.IBattery = float(data)
-    return msg
-
-def tx_TargetIBatt(msg, data):
-    msg.TX.TargetIBatt = float(data)
-    return msg
-
-def tx_IMaster(msg, data):
-    msg.TX.IMaster = float(data)
-    return msg
-
-def tx_ISlave1(msg, data):
-    msg.TX.ISlave1 = float(data)
-    return msg
-
-def tx_ISlave2(msg, data):
-    msg.TX.ISlave2 = float(data)
-    return msg
-
 ########################## RX ##########################
 
 def rx_EthIPAddr(msg, data):
@@ -134,50 +94,6 @@ def rx_Flags(msg, data):
 
 def rx_PowerLevel(msg, data):
     msg.RX.PowerLevel = turn_to_uint(data, 16) #turns to uint16
-    return msg
-
-def rx_VMon3v3(msg, data):
-    msg.RX.VMon3v3 = float(data)
-    return msg
-
-def rx_VMon5v(msg, data):
-    msg.RX.VMon5v = float(data)
-    return msg
-
-def rx_IMon5v(msg, data):
-    msg.RX.IMon5v = float(data)
-    return msg
-
-def rx_VMon12v(msg, data):
-    msg.RX.VMon12v = float(data)
-    return msg
-
-def rx_IMon12v(msg, data):
-    msg.RX.IMon12v = float(data)
-    return msg
-
-def rx_VMonGateDriver(msg, data):
-    msg.RX.VMonGateDriver = float(data)
-    return msg
-
-def rx_IMonGateDriver(msg, data):
-    msg.RX.IMonGateDriver = float(data)
-    return msg
-
-def rx_VMonPA(msg, data):
-    msg.RX.VMonPA = float(data)
-    return msg
-
-def rx_IMonPa(msg, data):
-    msg.RX.IMonPa = float(data)
-    return msg
-
-def rx_TMonPa(msg, data):
-    msg.RX.TMonPa = float(data)
-    return msg
-
-def rx_VMonBatt(msg, data):
-    msg.RX.VMonBatt = float(data)
     return msg
 
 def rx_VMonBattProg(msg, data):
@@ -208,14 +124,6 @@ def rx_IMaster(msg, data):
     msg.RX.IMaster = float(data)
     return msg
 
-def rx_ISlave1(msg, data):
-    msg.RX.ISlave1 = float(data)
-    return msg
-
-def rx_ISlave2(msg, data):
-    msg.RX.ISlave2 = float(data)
-    return msg
-
 ###################### Actual switch for funcs #####################
 
 def push_to_wibotic_msg(device, param, data, msg):
@@ -229,7 +137,6 @@ def push_to_wibotic_msg(device, param, data, msg):
             "PacketCount":tx_PacketCount,
             "ChargeState":tx_ChargeState,
             "Flags":tx_Flags,
-            "PowerLevel":tx_PowerLevel,
             "VMon3v3":tx_VMon3v3,
             "VMon5v":tx_VMon5v,
             "IMon5v":tx_IMon5v,
@@ -241,15 +148,6 @@ def push_to_wibotic_msg(device, param, data, msg):
             "IMonPa":tx_IMonPa,
             "TMonPa":tx_TMonPa,
             "VMonBatt":tx_VMonBatt,
-            "VMonBattProg":tx_VMonBattProg,
-            "VRect":tx_VRect,
-            "TBoard":tx_TBoard,
-            "ICharger":tx_ICharger,
-            "IBattery":tx_IBattery,
-            "TargetIBatt":tx_TargetIBatt,
-            "IMaster":tx_IMaster,
-            "ISlave1":tx_ISlave1,
-            "ISlave2":tx_ISlave2,
         }
     else:
         # msg.RX.header.stamp = ros_time
@@ -261,17 +159,6 @@ def push_to_wibotic_msg(device, param, data, msg):
             "ChargeState":rx_ChargeState,
             "Flags":rx_Flags,
             "PowerLevel":rx_PowerLevel,
-            "VMon3v3":rx_VMon3v3,
-            "VMon5v":rx_VMon5v,
-            "IMon5v":rx_IMon5v,
-            "VMon12v":rx_VMon12v,
-            "IMon12v":rx_IMon12v,
-            "VMonGateDriver":rx_VMonGateDriver,
-            "IMonGateDriver":rx_IMonGateDriver,
-            "VMonPA":rx_VMonPA,
-            "IMonPa":rx_IMonPa,
-            "TMonPa":rx_TMonPa,
-            "VMonBatt":rx_VMonBatt,
             "VMonBattProg":rx_VMonBattProg,
             "VRect":rx_VRect,
             "TBoard":rx_TBoard,
@@ -279,8 +166,6 @@ def push_to_wibotic_msg(device, param, data, msg):
             "IBattery":rx_IBattery,
             "TargetIBatt":rx_TargetIBatt,
             "IMaster":rx_IMaster,
-            "ISlave1":rx_ISlave1,
-            "ISlave2":rx_ISlave2,
         }
     func = switcher.get(param, "No function") #if param doesnt exist this should just return original msg
     if (func == "No function"):
